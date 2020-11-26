1f748b8f1159c62d1bee6f28676612812c1e1b6d
Yash Baheti <zilllionaire@gmail.com>
2020-11-26 14:34:41 +0530

    added configuration options

--- cli.js
+++ cli.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
 const express = require('express');
+const argv = require('yargs-parser')(process.argv.slice(2));
+const { existsSync } = require('fs');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxyConfig = require('./proxy.config');
-const port = +process.env.PORT || 3333;
+const { resolve } = require('path');
+
+const configFile = argv.config || 'proxy.config.js';
+const fullConfigPath = resolve(process.cwd(), configFile);
+
+if (!existsSync(fullConfigPath)) {
+    console.warn(`File not found at ${fullConfigPath}`);
+    return;
+}
+
+const proxyConfig = require(fullConfigPath);
+const port = +argv.port || 3333;
 const app = express();
 
 proxyConfig.forEach((item) => {
